c9fcd7889b7b23c37be7978fe68e9badc284b885
Imran Tachmuradov <30709221+DarkMagicED@users.noreply.github.com>
2023-11-18 13:34:27 -0800

    message

--- Calendar.py
+++ Calendar.py
@@ -1,11 +1,48 @@
+""" import calendar
+import datetime
+
+# Get the current year
+year = datetime.datetime.now().year
+
+# Create an empty string for the combined calendar
+combined_cal = ""
+
+# Loop over each month of the year
+for month in range(1, 13):
+    # Create a calendar object
+    cal = calendar.month(year, month)
+
+    # Append the calendar to the combined calendar
+    combined_cal += cal + "\n"
+
+# Print the combined calendar
+print(combined_cal)
+ """
 import calendar
+import datetime
+import tkinter as tk
 
-# Input year and month
-year = int(input("Enter year (e.g., 2023): "))
-month = int(input("Enter month (1-12): "))
+# Get the current year
+year = datetime.datetime.now().year
 
-# Create a calendar object
-cal = calendar.month(year, month)
+# Create an empty string for the combined calendar
+combined_cal = ""
 
-# Print the calendar
-print(cal)
+# Loop over each month of the year
+for month in range(1, 13):
+    # Create a calendar object
+    cal = calendar.month(year, month)
+
+    # Append the calendar to the combined calendar
+    combined_cal += cal + "\n"
+
+# Create a root window
+root = tk.Tk()
+
+# Create a Text widget and insert the combined calendar
+text = tk.Text(root)
+text.insert(tk.END, combined_cal)
+text.pack()
+
+# Start the tkinter event loop
+root.mainloop()
